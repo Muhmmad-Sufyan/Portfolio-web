f3b1642451a6cf4df8ced6da45a9f078872eb9da
Muhmmad-Sufyan <144341878+Muhmmad-Sufyan@users.noreply.github.com>
2024-11-05 17:55:35 +0500

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -10,6 +10,18 @@ export default function Home() {
       <Portfolio />
       <About />
       <Footer />  
+      <div className="flex justify-center items-center p-4 mt-10">
+      <div className="w-full max-w-lg aspect-[485/402]">
+        <iframe 
+          src="https://scratch.mit.edu/projects/141677189/embed" 
+          allowTransparency="true" 
+          allowFullScreen 
+          frameBorder="0" 
+          scrolling="no"
+          className="w-full h-full rounded-lg shadow-lg border border-gray-300"
+        />
+      </div>
+    </div>
     </>
   );
 }
